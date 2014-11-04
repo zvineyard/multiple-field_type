496f0eecf9596fd9df14dbefed3ad5a5ddaac56c
Ryan Thompson <service@aiwebsystems.com>
2014-11-04 02:00:03 -0600

    Rename

--- src/MultipleFieldType.php
+++ src/MultipleFieldType.php
@@ -1,8 +1,8 @@
 <?php namespace Anomaly\Streams\Addon\FieldType\Multiple;
 
-use Anomaly\Streams\Platform\Addon\FieldType\FieldTypeAddon;
+use Anomaly\Streams\Platform\Addon\FieldType\FieldType;
 use Anomaly\Streams\Platform\Stream\Model\StreamModel;
 
-class MultipleFieldType extends FieldTypeAddon
+class MultipleFieldType extends FieldType
 {
 }
