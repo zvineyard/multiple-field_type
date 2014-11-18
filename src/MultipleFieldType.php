44d28dd4e74cd1522c803e617b1867aec1ea9e98
Ryan Thompson <service@aiwebsystems.com>
2014-11-18 04:58:54 -0600

    TMP

--- src/MultipleFieldType.php
+++ src/MultipleFieldType.php
@@ -1,8 +1,37 @@
 <?php namespace Anomaly\Streams\Addon\FieldType\Multiple;
 
 use Anomaly\Streams\Platform\Addon\FieldType\FieldType;
-use Anomaly\Streams\Platform\Stream\Model\StreamModel;
+use Anomaly\Streams\Platform\Model\EloquentModel;
 
 class MultipleFieldType extends FieldType
 {
+
+    public function getRelation()
+    {
+        return $this->hasOne('Foo');
+    }
+
+    protected function getOptions()
+    {
+        $model = $this->getRelatedModel();
+
+        if (!$model instanceof EloquentModel) {
+
+            return [];
+        }
+
+        return $model->all()->lists('name', 'id');
+    }
+
+    protected function getRelatedModel()
+    {
+        $model = $this->getConfig('related');
+
+        if (!$model) {
+
+            return null;
+        }
+
+        return app()->make($model);
+    }
 }
