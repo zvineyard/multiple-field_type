7ba8a205ef4070bef189f93cec9c84e8d750aca0
Pepijn Olivier <olivier.pepijn@gmail.com>
2015-08-04 13:37:35 +0200

    made sort_order a nullable field

--- src/Listener/CreatePivotTable.php
+++ src/Listener/CreatePivotTable.php
@@ -56,7 +56,7 @@ class CreatePivotTable
 
                 $table->integer('entry_id');
                 $table->integer('related_id');
-                $table->integer('sort_order');
+                $table->integer('sort_order')->nullable();
 
                 $table->primary(['entry_id', 'related_id']);
             }
