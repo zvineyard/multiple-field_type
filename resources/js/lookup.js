e8ab9b22b10d5f0d2095971755fde7053a5483ad
Ryan Thompson <service@aiwebsystems.com>
2016-11-06 18:23:38 -0600

    Fix issue in JS where the initial selections were not sortable.

--- resources/js/lookup.js
+++ resources/js/lookup.js
@@ -1,7 +1,7 @@
-$(function() {
+$(function () {
 
     // Initialize multiple pickers
-    $('[data-provides="anomaly.field_type.multiple"]').each(function() {
+    $('[data-provides="anomaly.field_type.multiple"]').each(function () {
 
         var input = $(this);
         var field = input.data('field_name');
@@ -10,7 +10,7 @@ $(function() {
 
         var selected = $('[name="' + field + '"]').val().split(',');
 
-        modal.on('click', '[data-entry]', function(e) {
+        modal.on('click', '[data-entry]', function (e) {
 
             e.preventDefault();
 
@@ -20,69 +20,14 @@ $(function() {
 
             $(this).closest('tr').addClass('success').fadeOut();
 
-            wrapper.find('.selected').load(REQUEST_ROOT_PATH + '/streams/multiple-field_type/selected/' + $(this).data('key') + '?uploaded=' + selected.join(','), function() {
-                var adjustment;
-
-                var tree = $('.multiple-field_type .selected ul.tree.sortable').sortable({
-                    handle: '.handle',
-                    nested: false,
-                    onDragStart: function($item, container, _super, event) {
-                        $item.css({
-                            height: $item.outerHeight(),
-                            width: $item.outerWidth()
-                        });
-
-                        $item.addClass('dragged');
-
-                        $('body').addClass('dragging');
-
-                        adjustment = {
-                            left: container.rootGroup.pointer.left - $item.offset().left,
-                            top: container.rootGroup.pointer.top - $item.offset().top
-                        };
-
-                        _super($item, container);
-                    },
-                    onDrag: function($item, position) {
-                        $item.css({
-                            left: position.left - adjustment.left,
-                            top: position.top - adjustment.top
-                        });
-                    },
-                    onDrop: function($item, container, _super, event) {
-
-                        selected = [];
-
-                        $.each(tree.sortable('serialize').get()[0], function(key, item) {
-                            selected.push(String(item.id));
-                        });
-
-                        $('[name="' + field + '"]').val(selected.join(','));
-
-                        _super($item, container);
-                    },
-                    serialize: function($parent, $children, parentIsContainer) {
-
-                        var result = $.extend({}, $parent.data());
-
-                        if (parentIsContainer)
-                            return [$children];
-                        else if ($children[0]) {
-                            result.children = $children[0]; // This needs to return [0] for some reason..
-                        }
-
-                        delete result.subContainers;
-                        delete result.sortable;
-
-                        return result
-                    }
-                });
+            wrapper.find('.selected').load(REQUEST_ROOT_PATH + '/streams/multiple-field_type/selected/' + $(this).data('key') + '?uploaded=' + selected.join(','), function () {
+                wrapper.sort();
             });
 
             $(wrapper).find('[data-dismiss="multiple"]').removeClass('hidden');
         });
 
-        $(wrapper).on('click', '[data-dismiss="multiple"]', function(e) {
+        $(wrapper).on('click', '[data-dismiss="multiple"]', function (e) {
 
             e.preventDefault();
 
@@ -93,26 +38,29 @@ $(function() {
             $(this).closest('tr').addClass('danger').fadeOut();
         });
 
-        wrapper.find('.selected table').sortable({
-            handle: '.handle',
-            itemSelector: 'tr',
-            itemPath: '> tbody',
-            containerSelector: 'table',
-            placeholder: '<tr class="placeholder"/>',
-            afterMove: function($placeholder) {
+        wrapper.sort = function () {
+            wrapper.find('.selected table').sortable({
+                nested: false,
+                handle: '.handle',
+                itemSelector: 'tr',
+                itemPath: '> tbody',
+                containerSelector: 'table',
+                placeholder: '<tr class="placeholder"/>',
+                afterMove: function ($placeholder) {
 
-                $placeholder.closest('table').find('button.reorder').removeClass('disabled');
+                    $placeholder.closest('table').find('button.reorder').removeClass('disabled');
 
-                $placeholder.closest('table').find('.dragged').detach().insertBefore($placeholder);
+                    $placeholder.closest('table').find('.dragged').detach().insertBefore($placeholder);
 
-                selected = [];
+                    selected = [];
 
-                $(wrapper.find('table').find('[data-dismiss="multiple"]')).each(function() {
-                    selected.push(String($(this).data('entry')));
-                });
+                    $(wrapper.find('table').find('[data-dismiss="multiple"]')).each(function () {
+                        selected.push(String($(this).data('entry')));
+                    });
 
-                $('[name="' + field + '"]').val(selected.join(','));
-            }
-        });
+                    $('[name="' + field + '"]').val(selected.join(','));
+                }
+            });
+        }
     });
 });
