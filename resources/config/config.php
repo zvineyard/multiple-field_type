e35515bcb981dbda52d74579462fe21763a5bc42
Ryan Thompson <service@aiwebsystems.com>
2017-09-06 18:10:27 -0500

    Remove closures

--- resources/config/config.php
+++ resources/config/config.php
@@ -1,30 +1,13 @@
 <?php
 
-use Anomaly\Streams\Platform\Stream\Contract\StreamInterface;
+use Anomaly\MultipleFieldType\Support\Config\RelatedHandler;
 
 return [
     'related' => [
         'required' => true,
         'type'     => 'anomaly.field_type.select',
         'config'   => [
-            'options' => function (\Anomaly\Streams\Platform\Stream\Contract\StreamRepositoryInterface $streams) {
-
-                $options = [];
-
-                /* @var StreamInterface as $stream */
-                foreach ($streams->visible() as $stream) {
-                    $options[ucwords(str_replace('_', ' ', $stream->getNamespace()))][$stream->getEntryModelName(
-                    )] = $stream->getName();
-                }
-
-                foreach ($options as $namespace) {
-                    ksort($namespace);
-                }
-
-                ksort($options);
-
-                return $options;
-            },
+            'handler' => RelatedHandler::class,
         ],
     ],
     'mode'    => [
